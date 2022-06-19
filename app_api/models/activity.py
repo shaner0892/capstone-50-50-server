74d7ec3b94d871f1a06f512335f2d162946fd058
Hilary Shaner <hilaryshaner@gmail.com>
2022-06-19 16:02:19 -0500

    css changes

--- app_api/models/activity.py
+++ app_api/models/activity.py
@@ -5,7 +5,21 @@ class Activity(models.Model):
     title = models.CharField(max_length=50)
     state = models.ForeignKey("State", on_delete=models.CASCADE)
     city = models.CharField(max_length=20)
-    specific_location = models.CharField(max_length=100)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     is_approved = models.BooleanField()
     
+    # @property
+    # def average_rating(self):
+    #     """Average rating calculated attribute for each product
+    #     Returns:
+    #         number -- The average rating for the product
+    #     """
+    #     total_rating = 0
+    #     try:
+    #         for rating in self.ratings.all():
+    #             total_rating += rating.score
+
+    #         avg = total_rating / self.ratings.count()
+    #         return avg
+    #     except: 
+    #         return "No ratings yet"
--- app_api/models/fifty_user.py
+++ app_api/models/fifty_user.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class FiftyUser(models.Model):
 
     bio = models.CharField(max_length=300)
-    image_url = models.CharField(max_length=1000)
+    url = models.CharField(max_length=1000)
     location = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
